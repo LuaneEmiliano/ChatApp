9ab3266d34afc356c95a6497c26d3abaa3bfd44b
luane <luaneemiliano@gmail.com>
2022-07-02 18:15:40 -0700

    Initial Commit

---
+++ swift-chat/swift_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_chatApp.swift
+//  swift-chat
+//
+//  Created by luane Niejelski on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
